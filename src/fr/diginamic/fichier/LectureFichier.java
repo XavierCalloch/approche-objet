edeb201486ca0c725233b77cf14362b57ad33aa8
Xavier CALLOCH <callochxavier@gmail.com>
2020-03-10 11:20:06 +0100

    16e commit

---
+++ src/fr/diginamic/fichier/LectureFichier.java
@@ -0,0 +1,37 @@
+package fr.diginamic.fichier;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+public class LectureFichier {
+
+	public static void main(String[] args) { // throws IOException
+		
+		try {
+			File file = new File("C:/Users/xavie/Documents/recensement population 2016.csv");
+			List<String> lignes = FileUtils.readLines(file, "UTF-8");
+			
+			for (String ligne: lignes){
+				System.out.println(ligne);
+			}
+			
+			System.out.println("Nombre de lignes :" + lignes.size());
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		/*
+		for (int i = 1; lignes.size(); i++) {
+			
+		}
+		
+		String lignes = lignes.get(i);
+		*/
+		
+	}
+
+}
